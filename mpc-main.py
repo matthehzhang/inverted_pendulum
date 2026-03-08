732a4f787dd90ca4fde20e9b45c984bd6413dfc4
matthehzhang <vc.matthewzhang@gmail.com>
2026-03-08 08:00:42 -0500

    created m and c matricies

--- mpc-main.py
+++ mpc-main.py
@@ -85,16 +85,34 @@ def init():
     #find M and C for structure x = Mx0 + Cu
     
     #initialize matricies with 0
-    M = np.zeroes(2*horizon, 2) #each state takes 2 rows to match states (theta&v_angle)
+    M = np.zeros((2*horizon, 2)) #ea/ state takes 2 rows to match states (theta&v_angle)
                         #and 2 columns (effect of ea/ state from both theta0 & v_angle0)
-    C = np.zeroes(2*horizon, horizon) #added effect of input to each state's properties
+    C = np.zeros((2*horizon, horizon)) #added effect of input to each state's properties
 
-    
+    for j in range(horizon):
+        #run once for each predicted step state
+        #j=0,(0:2) filling x1, j=1,(2:4) filling x2
+        x_n1 = j*2
+        x_n2 = (j+1)*2
+        
+        M[x_n1:x_n2,:] = np.linalg.matrix_power(Ad, j+1) 
+        # ^ from x_n1 up to x_n2, all columns  ^ if j=0, Ad^1 | j=1, Ad^2 | j=2, Ad^3 ...
 
+        #m becomes...
+        #[Ad],[Ad^2],[Ad^3]
+        #where Ad is also a 2x2 matrix used to find the nxt state from cur state alone
+ 
+        for k in range(j+1):
+            #test0: j=0, k range(0->1), u_n1=0 u_n2=1 C[0:2,0:1] = [Bd]
+            #test1: j=1, k range(0->2), k=0 | u_n1=0, u_n2=1 C[2:4,0:1] = [AdBd]
+            #                         , k=1 | u_n1=1, u_n2=2 C[2:4,1:2] = [Bd]
+            u_n1 = k
+            u_n2 = k+1
+            C[x_n1:x_n2,u_n1:u_n2] = (np.linalg.matrix_power(Ad, j-k) @ Bd)
 
-    return (g,L,m,theta,dot_theta,dt,horizon,total_steps,A,B,x,x_id,Ad,Bd,u,Q,R,Qf,u_max)
+    return (g,L,m,theta,dot_theta,dt,horizon,total_steps,A,B,x,x_id,Ad,Bd,u,Q,R,Qf,u_max,M,C)
 
-def mpc_step (x,horizon,Ad,Bd,Q,R,Qf,u_max):
+def mpc_step (x,horizon,Ad,Bd,Q,R,Qf,u_max,M,C):
     
 
     return (u)
@@ -105,13 +123,13 @@ def plant_sim (x,u,g,L,m,dt):
     return()
 
 def main():
-    g,L,m,theta,dot_theta,dt,horizon,total_steps,A,B,x,x_id,Ad,Bd,u,Q,R,Qf,u_max = init()  
+    g,L,m,theta,dot_theta,dt,horizon,total_steps,A,B,x,x_id,Ad,Bd,u,Q,R,Qf,u_max,M,C = init()  
 
     #later, implement while loop, make theta start veritcally stable (=np.pi) and
     #implement swing-up control until its small angle then switch to mcp, would be cool
 
     for i in range(total_steps):
-        u = mpc_step(x,horizon,Ad,Bd,Q,R,Qf,u_max)
+        u = mpc_step(x,horizon,Ad,Bd,Q,R,Qf,u_max,M,C)
         x = plant_sim(x,u,g,L,m,dt)
     return
 
